72780b00e0a5c503e1f27e2fa2d448e052067d23
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-13 03:48:50 -0300

    Entendendo o OnInit

--- 02_lifecycle_hooks_angular/life-cycle/src/app/title/title.component.ts
+++ 02_lifecycle_hooks_angular/life-cycle/src/app/title/title.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.css']
+  styleUrls: ['./title.component.css'],
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit {
+  nome: string = 'pietro';
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.nome = `ola ${this.nome}`;
+  }
 }
