180ff9b6620af403051b40e5ff2144c8eed42c74
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-13 03:59:14 -0300

    OnChanges

--- 02_lifecycle_hooks_angular/life-cycle/src/app/title/title.component.ts
+++ 02_lifecycle_hooks_angular/life-cycle/src/app/title/title.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css'],
 })
-export class TitleComponent implements OnInit {
-  nome: string = 'pietro';
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() nome: string = '';
 
-  constructor() {}
+  constructor() {
+    console.log(`Construtor ${this.nome}`);
+  }
+
+  ngOnChanges(): void {
+    console.log(`OnChanges ${this.nome}`);
+  }
 
   ngOnInit(): void {
-    this.nome = `ola ${this.nome}`;
+    console.log(`OnInit ${this.nome}`);
   }
 }
