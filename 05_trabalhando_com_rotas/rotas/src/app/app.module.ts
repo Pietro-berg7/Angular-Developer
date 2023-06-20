c14e8b4133d91310ab8fdd860152a16f4952fe37
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-06-20 19:11:42 -0300

    Criando minhas primeiras rotas

--- 05_trabalhando_com_rotas/rotas/src/app/app.module.ts
+++ 05_trabalhando_com_rotas/rotas/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { IndexModule } from './pages/index/index.module';
+import { PortfolioModule } from './pages/portfolio/portfolio.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, IndexModule, PortfolioModule],
   providers: [],
   bootstrap: [AppComponent],
 })
