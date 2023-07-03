6e955cbcccab35375c92a226f75cded79a764cd3
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-07-02 23:47:30 -0300

    Pipe com Parâmetros

--- 07_trabalhando_com_pipes/pipes/src/app/pipes/transformador.pipe.ts
+++ 07_trabalhando_com_pipes/pipes/src/app/pipes/transformador.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'transformador',
 })
 export class TransformadorPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return value + ' abc';
+  transform(value: unknown, name: any, token: any): unknown {
+    return value + ' - ' + name + ' - ' + token;
   }
 }
