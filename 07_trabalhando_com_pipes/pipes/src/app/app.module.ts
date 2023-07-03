248978ca7907aac44aa36cb6c1578b17ff65feef
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-07-02 23:45:01 -0300

    Criando Pipes Customizados

--- 07_trabalhando_com_pipes/pipes/src/app/app.module.ts
+++ 07_trabalhando_com_pipes/pipes/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CardComponent } from './components/card/card.component';
 import { FormsModule } from '@angular/forms';
+import { TransformadorPipe } from './pipes/transformador.pipe';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent],
+  declarations: [AppComponent, CardComponent, TransformadorPipe],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ 07_trabalhando_com_pipes/pipes/src/app/pipes/transformador.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'transformador',
+})
+export class TransformadorPipe implements PipeTransform {
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return value + ' abc';
+  }
+}
