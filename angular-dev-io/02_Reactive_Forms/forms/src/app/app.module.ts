93f94773fa1f18b6ee51da7a38ac8649e24aa854
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-07-11 04:23:28 -0300

    Corrigindo erros

---
+++ angular-dev-io/02_Reactive_Forms/forms/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroComponent,
+    HomeComponent,
+    FooterComponent,
+    MenuComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
